a9cc3bc4764698af99a633469b7f6fba5a04a111
alvaro <alraled@hotmail.es>
2015-11-18 14:27:21 +0100

    Concesionario

---
+++ Concesionario/Controllers/TipoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Concesionario.Models;
+
+namespace Concesionario.Controllers
+{
+    public class TipoController : Controller
+    {
+        ConcesionarioEntities db = new ConcesionarioEntities();
+        // GET: Tipo
+        public ActionResult Index()
+        {
+            var data = db.Tipo;
+            return View(data);
+        }
+
+        public ActionResult Alta()
+        {
+            return View(new Tipo());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Tipo model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Tipo.Add(model);
+                db.SaveChanges();
+                return View(new Tipo());
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Detalle(int id)                   // ESTO ES PARA CAMBIAR EL NOMBRE DE LA URL //s
+        {
+
+
+            var data = db.Tipo.Find(id);
+            if (data == null)
+                return RedirectToAction("Index");
+            return View(data);
+        }
+
+    }
+
+
+
+}
---
+++ Concesionario/Controllers/VehiculoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Concesionario.Models;
+
+namespace Concesionario.Controllers
+{
+    public class VehiculoController : Controller
+    {
+
+        private ConcesionarioEntities db = new ConcesionarioEntities();
+
+        public ActionResult Index(int id)
+        {
+            var data = db.Vehiculo.Where(o=>o.idTipo == id);
+
+            return View(data);
+        }
+
+        //public ActionResult Detalle(String nombre) // ESTO ES PARA CAMBIAR EL NOMBRE DE LA URL //s
+        //{
+        //    var nom = nombre.Replace("_", " ");
+
+        //    var data = db.Vehiculo.FirstOrDefault(o => o.marca == nom);
+        //    if (data == null)
+        //        return RedirectToAction("Index");
+        //    return View(data);
+        //}
+
+        // GET: Producto
+        public ActionResult Alta()
+        {
+            return View(new Vehiculo());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Vehiculo model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Vehiculo.Add(model);
+                db.SaveChanges();
+                return View(new Vehiculo());
+            }
+
+            return View(model);
+        }
+    }
+}
